fba97ddf7dcc33b99033c3d985f068c053831319
unknown <chanhuicom@gmail.com>
2021-11-01 00:26:54 +0900

    매치 상세정보 API 업데이트

--- graphql/db.js
+++ graphql/db.js
@@ -1,29 +1,58 @@
 import fetch from "node-fetch";
-import { API_KEY } from "../config/key"
+import { API_KEY } from "../config/key";
 
 const API_URL = "https://api.nexon.co.kr/kart/v1.0/users/nickname/";
 const MATCH_INFO_URL = "https://api.nexon.co.kr/kart/v1.0/users/";
+const MATCH_MEMBER_INFO_URL = "https://api.nexon.co.kr/kart/v1.0/matches/";
 
 export const getUserId = (usrName) => {
   let REQUEST_URL = API_URL + encodeURI(usrName);
-  return fetch(REQUEST_URL,{
-    method: 'get',
+  return fetch(REQUEST_URL, {
+    method: "get",
     headers: {
-      'Authorization': API_KEY
-    }
+      Authorization: API_KEY,
+    },
   })
-  .then(res => res.json())
-  .then(json => json)
+    .then((res) => res.json())
+    .then((json) => json);
 };
 
 export const getMatch = (usrId) => {
   let REQUEST_MATCH_INFO_URL = MATCH_INFO_URL + usrId + "/matches?limit=200";
   return fetch(REQUEST_MATCH_INFO_URL, {
-    method: 'get',
-    headers:{
-      'Authorization': API_KEY
-    }
+    method: "get",
+    headers: {
+      Authorization: API_KEY,
+    },
   })
-  .then(res =>res.json())
-  .then(json=>json.matches);
-}
+    .then((res) => res.json())
+    .then((json) => json.matches);
+};
+
+export const getMatchMemberByIndi = (matchId) => {
+  let REQUEST_MATCH_MEMBER_INFO_URL = MATCH_MEMBER_INFO_URL + matchId;
+  return fetch(REQUEST_MATCH_MEMBER_INFO_URL, {
+    method: "get",
+    headers: {
+      Authorization: API_KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      return json.players;
+    });
+};
+
+export const getMatchMemberByTeam = (matchId) => {
+  let REQUEST_MATCH_MEMBER_INFO_URL = MATCH_MEMBER_INFO_URL + matchId;
+  return fetch(REQUEST_MATCH_MEMBER_INFO_URL, {
+    method: "get",
+    headers: {
+      Authorization: API_KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      return json.teams;
+    });
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,12 @@
-import { getUserId, getMatch } from "./db";
+import { getUserId, getMatch, getMatchMemberByTeam, getMatchMemberByIndi } from "./db";
 
 const resolvers = {
-  Query:{
-    user: (_, {usrName}) => getUserId(usrName),
-    matches: (_, {usrId}) => getMatch(usrId)
-  }
-}
+  Query: {
+    user: (_, { usrName }) => getUserId(usrName),
+    matches: (_, { usrId }) => getMatch(usrId),
+    matchTeamMember: (_, { matchId }) => getMatchMemberByTeam(matchId),
+    matchIndiMember: (_, { matchId }) => getMatchMemberByIndi(matchId),
+  },
+};
 
 export default resolvers;
